14900790fe6d8aa74191751643a7952556e52255
Denislav Temeliev <denis_1985@abv.bg>
2015-09-17 18:18:35 +0300

    added bullet and weapon

---
+++ src/models/spaceships/weapons/FlameStrike.java
@@ -0,0 +1,22 @@
+package models.spaceships.weapons;
+
+import audio.AudioAssets;
+import models.spaceships.weapons.bullets.*;
+import sun.audio.AudioPlayer;
+
+public class FlameStrike extends Weapon{
+
+    public FlameStrike(int cooldown, boolean shootingRight) {
+        super(cooldown, shootingRight);
+    }
+
+    @Override
+    public void shoot(int x, int y) {
+        if (this.cooldownCounter >= this.cooldown) {
+            AudioPlayer.player.start(AudioAssets.getSpaceshipLasserFourAudiInputStream());
+            Bullet bullet = new Fireball(x + 20, y + 40, this.shootingRight);
+            this.addBullet(bullet);
+            this.cooldownCounter = 0;
+        }
+    }
+}
---
+++ src/models/spaceships/weapons/bullets/Fireball.java
@@ -0,0 +1,15 @@
+package models.spaceships.weapons.bullets;
+
+import graphics.Assets;
+
+public class Fireball extends Bullet {
+    private static final String name = "Fireball";
+    private static final int width = 40;
+    private static final int height = 15;
+    private static final int damage = 400;
+    private static final int speed = 3;
+
+    public Fireball(int x, int y, boolean flyingRight) {
+        super(name, x, y, width, height, Assets.flameStrikeImage, damage, speed, flyingRight);
+    }
+}
